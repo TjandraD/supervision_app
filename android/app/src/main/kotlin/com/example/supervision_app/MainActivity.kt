23648c0484ac69c963fe665f05ef11f57f9741d4
TjandraD <tjandradarmo@protonmail.com>
2020-10-27 20:35:44 +0700

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/supervision_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.supervision_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(SupervisionApp());
+}
+
+class SupervisionApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
